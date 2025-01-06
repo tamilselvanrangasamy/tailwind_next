c83f1ad8418cdb41f67ae2fd2f8721de54de8f5c
tamilselvanrangasamy <tamilselvanrangasamy1@gmail.com>
2025-01-06 14:05:44 +0530

    Updates

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <>
+        <h1 className="">This is Blog page</h1>
+    </>
+  );
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div>
+        <h1 className="">This is Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
